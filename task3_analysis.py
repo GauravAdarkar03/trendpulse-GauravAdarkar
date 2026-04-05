0c1529d7e148d48e3c1bdb65345482e611319fa8
Gaurav Adarkar <adarkar.gaurav3317@gmail.com>
2026-04-05 11:09:01 +0530

    Update task3_analysis.py

--- task3_analysis.py
+++ task3_analysis.py
@@ -1,1 +1,19 @@
 
+#TASK 3 — Analyse Data (NumPy + Pandas)
+#What to analyse:
+#Average score
+#Most commented post
+#Top 5 posts
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data/clean_data.csv")
+
+print("Average Score:", np.mean(df["score"]))
+
+top_post = df.loc[df["score"].idxmax()]
+print("Top Post:", top_post["title"])
+
+top5 = df.sort_values(by="score", ascending=False).head(5)
+print(top5)
